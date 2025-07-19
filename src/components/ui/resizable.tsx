2b12385fa1a509acbcb04cc0b6207f734828bf2c
twessels <tjw74@pm.me>
2025-07-19 11:34:13 -0600

    ai wiring done

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -31,6 +31,9 @@ function ResizableHandle({
   className,
   ...props
 }: React.ComponentProps<typeof ResizablePrimitive.PanelResizeHandle>) {
+  // Destructure withHandle to prevent it from being passed to DOM
+  const { withHandle, ...restProps } = props as any;
+  
   return (
     <ResizablePrimitive.PanelResizeHandle
       data-slot="resizable-handle"
@@ -38,7 +41,7 @@ function ResizableHandle({
         "bg-border focus-visible:ring-ring relative flex w-px items-center justify-center after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:ring-1 focus-visible:ring-offset-1 focus-visible:outline-hidden data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:translate-x-0 data-[panel-group-direction=vertical]:after:-translate-y-1/2 [&[data-panel-group-direction=vertical]>div]:rotate-90",
         className
       )}
-      {...props}
+      {...restProps}
     >
       {/* No visible handle indicator */}
     </ResizablePrimitive.PanelResizeHandle>
